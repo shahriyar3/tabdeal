fc05e9602721205859d2765f093725f3162c4d4c
Jan Linhart <admin@escope.cz>
2024-12-23 19:47:05 +0100

    null cannot be a return type

--- app/bundles/UserBundle/Security/DummyToken.php
+++ app/bundles/UserBundle/Security/DummyToken.php
@@ -8,8 +8,8 @@ use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
 final class DummyToken extends AbstractToken
 {
-    public function getCredentials(): null
+    public function getCredentials(): string
     {
-        return null;
+        return '';
     }
 }
