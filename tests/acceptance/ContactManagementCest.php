449a87361cc87a1ca8dbf89565c2b67de1d41288
Saurabh Gupta <saurabh.gupta@acquia.com>
2025-03-25 18:35:40 +0530

    add timeout

--- tests/acceptance/ContactManagementCest.php
+++ tests/acceptance/ContactManagementCest.php
@@ -255,7 +255,7 @@ class ContactManagementCest
         $I->click(CampaignPage::$contactsTab);
 
         // Verify that the first and second contacts are now in the campaign
-        $I->waitForElementVisible(CampaignPage::$firstContactFromContactsTab);
+        $I->waitForElementVisible(CampaignPage::$firstContactFromContactsTab, 30);
         $I->see($contactName1, CampaignPage::$firstContactFromContactsTab);
         $I->see($contactName2, CampaignPage::$secondContactFromContactsTab);
     }
